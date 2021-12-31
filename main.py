17da1a02db30b2d6fe3312f95c17cb00f3510b1f
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-12-31 21:35:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,6 @@
 
 # Shell prototype
 from lib.core import *
-import pyttsx3
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
-
 
 # The code segment below is for
 # setting up the path in android
